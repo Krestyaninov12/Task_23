0b3c1e10743ba3f009118277d05902286ea68621
Krestyaninov12 <kreda@pnpe.ru>
2022-01-31 01:39:32 +0300

    Добавьте файлы проекта.

---
+++ Task_23/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для вычисления факториала: ");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            Task _ = FactorialAsync(f);
+                        
+            for (int i = 0; i <= f; i++)
+            {
+                Console.WriteLine('*');
+                Thread.Sleep(700);
+            }
+            Console.ReadKey();
+        }
+        static async Task FactorialAsync(int num)
+        {
+            Console.WriteLine("Этапы расчета:");
+            int res = await Task.Run(() =>
+            {
+                int result = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    result *= i;
+                    Console.WriteLine($"{result,10}");
+                    Thread.Sleep(500);
+                }
+                return result;
+            });
+            Console.WriteLine($"Факториал числа равен {res}");
+        }
+    }
+}
